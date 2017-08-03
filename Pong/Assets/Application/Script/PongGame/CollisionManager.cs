536c0e38f483c4916bf03f55f1c74ce79123fc55
THS50111\sugou.masayuki <a>
2017-08-03 15:50:08 +0900

    コリジョンマネージャ土台作成

---
+++ Pong/Assets/Application/Script/PongGame/CollisionManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CollisionManager : MonoBehaviour
+{
+    public Wall wall;
+    public Ball ball;
+    public bool hitPlayer1;
+    public bool hitPlayer2;
+    public int hitWall;
+
+    private int ballState;
+    private int wallState;
+
+    // Use this for initialization
+    void Start()
+    {
+        hitPlayer1 = false;
+        hitPlayer2 = false;
+        hitWall = 0;
+        ballState = 0;
+        wallState = 0;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //当たった場合の処理
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Racket1")
+        {//ボールと1Pのラケットが当たった場合
+            hitPlayer1 = true;
+            hitPlayer2 = false;
+        }
+        else if (col.gameObject.tag == "Racket2")
+        {//ボールと2Pのラケットが当たった場合
+            hitPlayer1 = false;
+            hitPlayer2 = true;
+        }
+        else if (col.gameObject.tag == "Wall")
+        {//ボールと壁が当たった場合
+            wallState = wall.GetWallState();
+            ballState = ball.GetBallState();
+
+            if(ballState == 0)
+            {
+            }
+            else if(wallState != ballState)
+            {
+                if (wallState == 0)
+                {
+                    wallState = ballState;
+                    wall.SetWallState(wallState);
+                }
+                else
+                {
+                    wallState = ballState;
+                    wall.SetWallState(wallState);
+                    //ball.SpeedDown(); //減速
+                }
+            }
+            else
+            {
+                //加速
+            }
+        }
+    }
+}
