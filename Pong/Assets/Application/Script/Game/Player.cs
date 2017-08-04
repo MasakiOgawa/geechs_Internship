e8593e764a2a885679c2a25af6e7b8d1cd6f31fa
Shibasaki <a>
2017-08-04 14:59:43 +0900

    playerの追加

---
+++ Pong/Assets/Application/Script/Game/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	//public class//
+	public ResultController resultcontroller;
+	public GaugeController gaugecontroller;
+	public float Speed;
+	public int Rote;
+	//private class//
+	private int Set_Score;
+	private int ResolutScore;
+
+	// Use this for initialization
+	void Start () {
+		Set_Score = 0;
+		ResolutScore = 0;
+		Speed = 1f;
+		Rote = 15;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W) && this.transform.position.z < 23) {
+			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, 
+				this.transform.position.z + Speed);
+			
+			this.transform.rotation = Quaternion.Euler(0, 21, 0);
+		}
+		else if (Input.GetKey(KeyCode.S) && this.transform.position.z > -23) {
+			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, 
+				this.transform.position.z - Speed);
+			this.transform.rotation = Quaternion.Euler(0, -21, 0);
+		} 
+		if (Input.GetKeyUp(KeyCode.W)) {
+			this.transform.rotation = Quaternion.Euler(0, 0, 0);
+		}
+		if (Input.GetKeyUp(KeyCode.S)) {
+			this.transform.rotation = Quaternion.Euler(0, 0, 0);
+		}
+			
+	}
+	void AddGauge(){
+		gaugecontroller.Score = Set_Score;
+	}
+}
---
+++ Pong/Assets/Application/Script/Game/Player2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour {
+	
+	//public class//
+	public ResultController resultcontroller;
+	public GaugeController gaugecontroller;
+	public float Speed;
+	public int Rota;
+
+	//private class//
+	private int Set_Score;
+	private int ResolutScore;
+
+	// Use this for initialization
+	void Start () {
+		Set_Score = 0;
+		ResolutScore = 0;
+		Speed = 1;
+		Rota = 3;
+	}
+
+	// Update is called once per frame
+	//ラケット操作//
+	void Update () {
+		//ラケット上移動//
+		Vector3 euler = transform.rotation.eulerAngles;
+
+		if (Input.GetKey (KeyCode.UpArrow) && this.transform.position.z < 23) {
+			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, 
+				this.transform.position.z + Speed);
+
+			//ラケット回転//		
+			this.transform.rotation = Quaternion.Euler (0, -21, 0);
+
+			//ラケット下移動//
+		} else if (Input.GetKey (KeyCode.DownArrow) && this.transform.position.z > -23) {
+			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, 
+				this.transform.position.z - Speed);
+			//ラケット回転//
+			this.transform.rotation = Quaternion.Euler (0, 21, 0);
+		}
+		//ラケット回転リセット//
+		if (Input.GetKeyUp (KeyCode.UpArrow)) {
+				this.transform.rotation = Quaternion.Euler (0, 0, 0);
+			}
+		if (Input.GetKeyUp (KeyCode.DownArrow)) {
+				this.transform.rotation = Quaternion.Euler (0, 0, 0);
+			}
+
+		}
+	//スコア//
+	void AddGauge(){
+		gaugecontroller.Score = Set_Score;
+	}
+}
