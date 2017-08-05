d0f528604ebebdb0ff697838973840c0f7bf4ead
将輝 小川 <jrp6exu3f@outlook.jp>
2017-08-05 14:55:50 +0900

    ・ゲームパッド対応 ・打ち返すときにジャスト入力で強返し ・各数値調整

--- Pong/Assets/Application/Script/Game/Player.cs
+++ Pong/Assets/Application/Script/Game/Player.cs
@@ -2,50 +2,123 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+	// コンスト定数
+	private const float PLAYER_MOVE_MAX = 23.0f;        // プレイヤーの移動最大値
+	private const int MOVE_COUNT_MAX = 5;               // 移動カウンタ最大値
+	private const float PLAYER_ANGLE = 22.0f;           // プレイヤーの角度
+	private const float BALL_SPEED_UP = 1.5f;			// ボール加速量
+
 	//public class//
 	public ResultController resultcontroller;
 	public GaugeController gaugecontroller;
 	public float Speed;
 	public int Rote;
+
 	//private class//
 	private int Set_Score;
-	private int ResolutScore;
+	private int MoveCount;		// プレイヤーが動き出す時間計測用
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		Set_Score = 0;
-		ResolutScore = 0;
 		Speed = 1f;
 		Rote = 15;
-
+		MoveCount = 0;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.W) && this.transform.position.z < 23) {
-			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, 
-				this.transform.position.z + Speed);
-			
-			this.transform.rotation = Quaternion.Euler(0, 21, 0);
+	void Update()
+	{
+		// 上方向移動
+		if (Input.GetAxisRaw("Vertical1") > 0)
+		{
+			// 角度変更
+			transform.rotation = Quaternion.Euler(0, PLAYER_ANGLE, 0);
+
+			// 一定以上長押しされたら移動
+			if (MoveCount >= MOVE_COUNT_MAX)
+			{
+				transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Speed);
+
+				// 上限
+				if (transform.position.z > PLAYER_MOVE_MAX)
+				{
+					transform.position = new Vector3(transform.position.x, transform.position.y, PLAYER_MOVE_MAX);
+				}
+			}
+			// カウンタが最大値未満なら加算
+			else if(MoveCount < MOVE_COUNT_MAX)
+			{
+				// カウンタ加算
+				MoveCount++;
+			}
 		}
-		else if (Input.GetKey(KeyCode.S) && this.transform.position.z > -23) {
-			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, 
-				this.transform.position.z - Speed);
-			this.transform.rotation = Quaternion.Euler(0, -21, 0);
-		} 
-		if (Input.GetKeyUp(KeyCode.W)) {
-			this.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+		// 下方向移動
+		if (Input.GetAxisRaw("Vertical1") < 0)
+		{
+			// 角度変更
+			transform.rotation = Quaternion.Euler(0, -PLAYER_ANGLE, 0);
+
+			// 一定以上長押しされたら移動
+			if (MoveCount >= MOVE_COUNT_MAX)
+			{
+				transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - Speed);
+				
+				// 下限
+				if (transform.position.z < -PLAYER_MOVE_MAX)
+				{
+					transform.position = new Vector3(transform.position.x, transform.position.y, -PLAYER_MOVE_MAX);
+				}
+			}
+			// カウンタが最大値未満なら加算
+			else if (MoveCount < MOVE_COUNT_MAX)
+			{
+				// カウンタ加算
+				MoveCount++;
+			}
 		}
-		if (Input.GetKeyUp(KeyCode.S)) {
-			this.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+		// 上下の入力がされていないとき
+		if (Input.GetAxisRaw("Vertical1") == 0)
+		{
+			transform.rotation = Quaternion.Euler(0, 0, 0);
+			MoveCount = 0;
 		}
-			
 	}
 
-	public void SetScore (int Score)
+
+	//=============================================================================
+	// スコア設定関数
+	//=============================================================================
+	public void SetScore(int Score)
 	{
+		// 現在のスコアに加算
 		Set_Score += Score;
+
+		// ゲージに現在のスコアを設定
 		gaugecontroller.SetGaugeValue(Set_Score);
+	}
+
+	//=============================================================================
+	// 衝突判定がされたら
+	//=============================================================================
+	private void OnCollisionEnter(Collision collision)
+	{
+		// 衝突対象がボール
+		if(collision.gameObject.tag == "Ball")
+		{
+			// プレイヤーが動かずはじいたらボールの勢いを加算
+			if(MoveCount < MOVE_COUNT_MAX)
+			{
+				Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+				rb.velocity *= BALL_SPEED_UP;
+			}
+		}
+		
 	}
 }
--- Pong/Assets/Application/Script/Game/Player2.cs
+++ Pong/Assets/Application/Script/Game/Player2.cs
@@ -1,65 +1,124 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Player2 : MonoBehaviour {
-	
+public class Player2 : MonoBehaviour
+{
+	// コンスト定数
+	private const float PLAYER_MOVE_MAX = 23.0f;        // プレイヤーの移動最大値
+	private const int MOVE_COUNT_MAX = 5;				// 移動カウンタ最大値
+	private const float PLAYER_ANGLE = 22.0f;           // プレイヤーの角度
+	private const float BALL_SPEED_UP = 1.5f;           // ボール加速量
+
 	//public class//
 	public ResultController resultcontroller;
 	public GaugeController gaugecontroller;
 	public float Speed;
-	public int Rota;
+	public int Rote;
 
 	//private class//
 	private int Set_Score;
-	private int ResolutScore;
+	private int MoveCount;      // プレイヤーが動き出す時間計測用
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		Set_Score = 0;
-		ResolutScore = 0;
-		Speed = 1;
-		Rota = 3;
+		Speed = 1f;
+		Rote = 15;
+		MoveCount = 0;
 	}
 
 	// Update is called once per frame
-	//ラケット操作//
 	void Update()
 	{
-		//ラケット上移動//
-		Vector3 euler = transform.rotation.eulerAngles;
+		// 上方向移動
+		if (Input.GetAxisRaw("Vertical2") > 0)
+		{
+			// 角度変更
+			transform.rotation = Quaternion.Euler(0, -PLAYER_ANGLE, 0);
 
-		if (Input.GetKey(KeyCode.UpArrow) && this.transform.position.z < 23)
-		{
-			this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y,
-				this.transform.position.z + Speed);
+			// 一定以上長押しされたら移動
+			if (MoveCount >= MOVE_COUNT_MAX)
+			{
+				transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Speed);
 
-			//ラケット回転//		
-			this.transform.rotation = Quaternion.Euler(0, -21, 0);
-
-			//ラケット下移動//
-		}
-		else if (Input.GetKey(KeyCode.DownArrow) && this.transform.position.z > -23)
-		{
-			this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y,
-				this.transform.position.z - Speed);
-			//ラケット回転//
-			this.transform.rotation = Quaternion.Euler(0, 21, 0);
-		}
-		//ラケット回転リセット//
-		if (Input.GetKeyUp(KeyCode.UpArrow))
-		{
-			this.transform.rotation = Quaternion.Euler(0, 0, 0);
-		}
-		if (Input.GetKeyUp(KeyCode.DownArrow))
-		{
-			this.transform.rotation = Quaternion.Euler(0, 0, 0);
+				// 上限
+				if (transform.position.z > PLAYER_MOVE_MAX)
+				{
+					transform.position = new Vector3(transform.position.x, transform.position.y, PLAYER_MOVE_MAX);
+				}
+			}
+			// カウンタが最大値未満なら加算
+			else if (MoveCount < MOVE_COUNT_MAX)
+			{
+				// カウンタ加算
+				MoveCount++;
+			}
 		}
 
+		// 下方向移動
+		if (Input.GetAxisRaw("Vertical2") < 0)
+		{
+			// 角度変更
+			transform.rotation = Quaternion.Euler(0, PLAYER_ANGLE, 0);
+
+			// 一定以上長押しされたら移動
+			if (MoveCount >= MOVE_COUNT_MAX)
+			{
+				transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - Speed);
+
+				// 下限
+				if (transform.position.z < -PLAYER_MOVE_MAX)
+				{
+					transform.position = new Vector3(transform.position.x, transform.position.y, -PLAYER_MOVE_MAX);
+				}
+			}
+			// カウンタが最大値未満なら加算
+			else if (MoveCount < MOVE_COUNT_MAX)
+			{
+				// カウンタ加算
+				MoveCount++;
+			}
+		}
+
+		// 上下の入力がされていないとき
+		if (Input.GetAxisRaw("Vertical2") == 0)
+		{
+			transform.rotation = Quaternion.Euler(0, 0, 0);
+			MoveCount = 0;
+		}
 	}
 
+
+	//=============================================================================
+	// スコア設定関数
+	//=============================================================================
 	public void SetScore(int Score)
 	{
+		// 現在のスコアに加算
 		Set_Score += Score;
+
+		// ゲージに現在のスコアを設定
 		gaugecontroller.SetGaugeValue(Set_Score);
+	}
+
+	//=============================================================================
+	// 衝突判定がされたら
+	//=============================================================================
+	private void OnCollisionEnter(Collision collision)
+	{
+		// 衝突対象がボール
+		if (collision.gameObject.tag == "Ball")
+		{
+			// プレイヤーが動かずはじいたらボールの勢いを加算
+			if (MoveCount < MOVE_COUNT_MAX)
+			{
+				Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+				rb.velocity *= BALL_SPEED_UP;
+			}
+		}
+
 	}
 }
